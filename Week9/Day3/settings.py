caf756f49ec24f6c0e80a368cc647d3e4e65d06f
Aryan-Hestabit <aryan.hestabit@gmail.com>
2026-03-31 16:32:44 +0530

    made changes

--- Week9/Day3/settings.py
+++ Week9/Day3/settings.py
@@ -1,6 +1,6 @@
 import os
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyCQtlUs-hvBxWZiXYQjg54kuVL8cQd5kgU")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "Your_Gemini_API_Key_Here")
 MODEL_ID = "gemini-3.1-flash-lite-preview"
 
 
